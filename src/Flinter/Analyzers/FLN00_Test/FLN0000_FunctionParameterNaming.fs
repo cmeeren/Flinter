6f2a26336ee154ca7ebe8533d57ebcbbbed15e2f
Christer van der Meeren <christer@infotech.no>
2022-12-28 20:29:38 +0100

    Add POC test rule

---
+++ src/Flinter/Analyzers/FLN00_Test/FLN0000_FunctionParameterNaming.fs
@@ -0,0 +1,31 @@
+module Flinter.Analyzers.FLN0000_FunctionParameterNaming
+
+open System
+open FSharp.Analyzers.SDK
+open FSharp.Compiler.Syntax
+open Flinter.Analyzers
+open Flinter.Ast
+
+
+let private createMsg (i: Ident) =
+    Message.Create($"The parameter name '%s{i.idText}' does not follow the rule for parameter names.", i.idRange)
+
+
+let private isInvalid (i: Ident) = i.idText.Chars(0) |> Char.IsUpper
+
+
+type private Visitor(addMsg) =
+    inherit UntypedAstVisitor()
+
+    override this.VisitSynBinding(node, path) =
+        match node with
+        | SynBinding(_, _, _, _, _, _, SynValData(_, SynValInfo(curriedArgInfos, _), _), _, _, _, _, _) ->
+            curriedArgInfos
+            |> List.iter (List.iter (fun i -> i.Ident |> Option.filter isInvalid |> Option.iter (createMsg >> addMsg)))
+
+        base.VisitSynBinding(node, path)
+
+
+// TODO: Specify analyzer name in attribute? What are these names used for?
+[<Analyzer>]
+let analyze: Analyzer = createUntypedAnalyzer Visitor
---
+++ src/Flinter/Analyzers/Helpers.fs
@@ -0,0 +1,42 @@
+[<AutoOpen>]
+module Flinter.Analyzers.Helpers
+
+open System.IO
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open FSharp.Analyzers.SDK
+open Flinter.Ast
+
+
+type Message with
+
+    static member Create
+        (
+            message,
+            range,
+            severity,
+            [<CallerFilePath; Optional; DefaultParameterValue("")>] ruleFilePath: string
+        ) =
+        let fn = Path.GetFileNameWithoutExtension(ruleFilePath)
+        let parts = fn.Split('_')
+
+        {
+            Code = parts[0]
+            Type = parts[1]
+            Message = message
+            Severity = severity
+            Range = range
+            Fixes = []
+        }
+
+    static member Create(message, range, [<CallerFilePath; Optional; DefaultParameterValue("")>] ruleFilePath: string) =
+        Message.Create(message, range, Severity.Warning, ruleFilePath)
+
+
+
+let createUntypedAnalyzer (getVisitor: _ -> #UntypedAstVisitor) : Analyzer =
+    fun (context: Context) ->
+        let messages = ResizeArray()
+        let visitor = getVisitor messages.Add
+        visitor.VisitParsedInput context.ParseTree
+        messages |> Seq.toList
